86d658cea206a9c0fa24f493aca3db267874e31a
Brenton Holswich <brenton.holswich@gmail.com>
2022-08-05 06:21:32 -0700

    added calculator and functions to render it

--- index.js
+++ index.js
@@ -1,9 +1,52 @@
-var http = require("http");
+import http from 'http'
+import {
+  sum,
+  divide,
+  multiply,
+  toThePower,
+  takeAway,
+  squareRoot,
+  square,
+} from './src/calculator'
+
+function wrapInHtml(title, toPrint) {
+  return `<div>${title}: ${toPrint}</div>`
+}
+
+function createDocument(body) {
+  return `<!DOCTYPE html>
+  <html>
+  
+  <head>
+    <title>VS Code Debug</title>
+    <meta charset="UTF-8" />
+  </head>
+  
+  <body>
+    ${body}
+  </body>
+  
+  </html>`
+}
+
+function createBodyHtml() {
+  return `<main>
+  ${wrapInHtml('sum', sum(1, 2, 3, 4, 5))}
+  ${wrapInHtml('subtract', takeAway(1, 2, 3, 4, 5))}
+  ${wrapInHtml('divide', divide(1, 2, 3, 4, 5))}
+  ${wrapInHtml('multiply', multiply(1, 2, 3, 4, 5))}
+  ${wrapInHtml('square', square(3))}
+  ${wrapInHtml('squareRoot', squareRoot(9))}
+  ${wrapInHtml('toThePower', toThePower(9, 3))}
+  </main>`
+}
 
 //create a server object:
 http
-  .createServer(function(req, res) {
-    res.write("Hello World!"); //write a response to the client
-    res.end(); //end the response
+  .createServer(function (req, res) {
+    const body = createBodyHtml()
+    const formatResponse = createDocument(body)
+    res.write(formatResponse) //write a response to the client
+    res.end() //end the response
   })
-  .listen(8080); //the server object listens on port 8080
+  .listen(8080) //the server object listens on port 8080
---
+++ src/calculator.js
@@ -0,0 +1,105 @@
+// sum,divide,multiply,toThePower,takeAway,squareRoot,square
+let result
+
+/**
+ * Adds two numbers to each other and returns the result
+ * @param {number} a first number to add
+ * @param {number} b second number to add
+ * @returns {number} the result of adding a and b together
+ */
+function addTwoNumbersTogether(a, b) {
+  result += a + b
+  return result
+}
+/**
+ * add all numbers passed in
+ * @param {number[]} arrayOfNumbers takes numbers as args to add together
+ * @returns {number}
+ */
+function sum(arrayOfNumbers) {
+  const numbersToSum = [...arrayOfNumbers, ...args]
+  for (let i = numbersToSum.length; i > 0; i--) {
+    result = 0
+    result = addTwoNumbersTogether(result, numbersToSum[i])
+  }
+  return result
+}
+
+/**
+ * inverse of sum - subtract all numbers passed in
+ * @param {number[]} arrayOfNumbers numbers to subtract
+ * @returns {number} result of subtracting all numbers in the array
+ */
+function takeAway(arrayOfNumbers) {
+  const numbersToTakeAway = [...arrayOfNumbers, args]
+  result = 0
+  for (let i = 0; i < numbersToTakeAway; i++) {
+    result -= result - numbersToTakeAway[i]
+  }
+  return result
+}
+
+/**
+ * multiplies all numbers passed in
+ * @param {number[]} arrayOfNumbers numbers to multiply
+ * @returns {number} result of multiplying all numbers in the array
+ */
+function multiply(arrayOfNumbers) {
+  const numbersToMultiply = [...arrayOfNumbers, args]
+  result = 0
+  for (let i = 0; i < numbersToMultiply; i++) {
+    result *= result * numbersToMultiply[i]
+  }
+  return result
+}
+
+/**
+ * Divide all numbers passed in
+ * @param {number[]} arrayOfNumbers numbers to divide
+ * @returns {number} result of dividing all numbers in the array
+ */
+function divide(arrayOfNumbers) {
+  const numbersToTakeAway = [...arrayOfNumbers, args]
+  result = 0
+  for (let i = 0; i < numbersToTakeAway; i++) {
+    result /= numbersToTakeAway[i]
+  }
+  return result
+}
+
+/**
+ * square of the number passed in
+ * @param {number} toSquare number to square
+ * @returns {number} result of squaring the number
+ */
+function square(toSquare) {
+  result = multiply(toSquare, toSquare)
+  return result
+}
+
+/**
+ * square root of passed in
+ * @param {number} square number to get square root of
+ * @returns {number} the square root of the number passed in
+ */
+function squareRoot(square) {
+  result = 0
+  for (let i = 0; i < numbersToTakeAway; i++) {
+    result = result * numbersToTakeAway[i]
+  }
+  return result
+}
+
+/**
+ * raise base to the power
+ * @param {number} base base
+ * @param {number} power power
+ * @returns {number} result of raising base to the power
+ */
+function toThePower(base, power) {
+  result = 0
+  for (let i = power; i > 0; --i) {
+    result = multiply(base, base)
+  }
+  return result
+}
